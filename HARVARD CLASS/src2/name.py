76c90e6a68b50a84421f91fb22d68498914f705d
Moses <Mosesajibade97@gmail.com>
2020-05-06 20:08:06 -0400

    new

--- HARVARD CLASS/src2/name.py
+++ HARVARD CLASS/src2/name.py
@@ -5,3 +5,4 @@ print(f"hello, {name}!")
 # the inout funtion allows me to put down whatever i want. 
 # the squigly line is like a blank variabke that allows me to input what i want yto print in the terminal
 # the f stands for format strings. that lets us subtitite strinsg with any information that we want.
+#it works by inserting my input into the terminal
